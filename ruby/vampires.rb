7b437defea472dc0b3cccc8a4314e2a84f7fa5f8
Kim Stephenson <kmstephe@usc.edu>
2016-07-11 23:42:14 -0700

    Add conditional statements

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,6 +1,39 @@
-def vampire_detector
-  puts "What is your name?"
-  puts "How old are you? What year were you born?"
-  puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-  puts "Would you like to enroll in the company's health insurance?"
+puts "What is your name?"
+  name = gets.chomp
+puts "How old are you?"
+  age = gets.chomp.to_i
+puts "What year were you born?"
+  birth_year = gets.chomp.to_i
+  if age == 2016 - birth_year
+    correct_age = true
+  else
+    correct_age = false
+  end
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+  wants_garlicbread = gets.chomp
+  if wants_garlicbread == "y"
+    wants_garlicbread = true
+  elsif wants_garlicbread == "n"
+    wants_garlicbread = false
+  end
+puts "Would you like to enroll in the company's health insurance? (y/n)"
+  insurance = gets.chomp
+  if insurance == "y"
+    insurance = true
+  elsif insurance == "n"
+    insurance = false
+  end
+
+
+case
+when correct_age && wants_garlicbread, correct_age && insurance
+    puts "Probably not a vampire"
+when !correct_age && !wants_garlicbread && insurance, !correct_age && !insurance && wants_garlicbread
+    puts "Probably a vampire"
+when !correct_age && (!wants_garlicbread && !insurance)
+    puts "Almost certainly a vampire"
+when name == "Tu Fang", name == "Drake Cula"
+    puts "Definitely a vampire"
+else
+  puts "Results inconclusive"
 end
