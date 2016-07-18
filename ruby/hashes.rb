ff8e8ddafceec830daaf04050fd669c6c8a9e7fe
Kim Stephenson <kmstephe@usc.edu>
2016-07-18 13:08:13 -0700

    Add client info hash

---
+++ ruby/hashes.rb
@@ -0,0 +1,16 @@
+#Define a hash to store the information
+
+client_info = {
+  name: ""
+  age: ""
+  children: ""
+  decor_theme: ""
+  email: ""
+  wants_coupon: ""
+}
+
+#Ask user for each type of information, then convert to the right data type and store to appropriate key
+
+#Allow user to update a key, or enter "none" to skip
+
+#Print the hash
