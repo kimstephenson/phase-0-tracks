453a244ecc7095491b0f19fcd31e8630f92bb2e3
Kim Stephenson <kmstephe@usc.edu>
2016-07-18 16:44:32 -0700

    Populate structures with data

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,35 @@
+zoo = {
+  mammals: {
+    cats: [
+      "tiger",
+      "lion",
+      "cheetah"
+      ],
+    misc: {
+      dog: "hyena",
+      marine_mammal: "dolphin",
+      bear: "grizzly"
+    }
+  },
+  birds: {
+    large: [
+      "emu",
+      "ostrich"
+    ],
+    small: [
+      "finch",
+      "parakeet"
+    ],
+    medium: "macaw"
+    },
+  reptiles: {
+    snakes: [
+      "python",
+      "cobra"
+    ],
+    misc: {
+      lizard: "monitor",
+      turtle: "snapping turtle"
+    }
+  }
+}
