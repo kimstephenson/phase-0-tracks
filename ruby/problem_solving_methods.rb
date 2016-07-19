1baeccb49eac81af506000d8af91139b02380f47
Kim Stephenson <kmstephe@usc.edu>
2016-07-19 16:05:35 -0700

    Bug fixes

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,19 +1,23 @@
 arr = [42, 89, 23, 1]
 
 def search_array(arr, number)
-  arr.index(number)
+  arr.each do |n|
+    if n == number
+      p (arr.length - 1)
+    end
+  end
 end
 
-p search_array(arr, 32)
-p search_array(arr, 1)
+search_array(arr, 32)
+search_array(arr, 89)
 
 def fibonacci_adder(x)
   array = [0,1,1,2,3,5]
   if x >= 7
     (x-6).times do
       array << (array[-1] + array[-2])
-      p array
     end
+    p array
   else
     p array[0...x]
   end
