6bf20fceae8e1dbf60d6fcd4b67a1336ac1bbfa6
Kim Stephenson <kmstephe@usc.edu>
2016-07-19 11:48:34 -0700

    Add arrays with each and map! methods

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,3 +1,5 @@
+# Release 0
+
 def food
   puts "I'm hungry."
   yield("pizza", "ice cream", "haagen daz")
@@ -5,3 +7,33 @@ def food
 end
 
 food { |food1, food2, food3| puts "I want some #{food1} and #{food2} but only if it's #{food3}!"}
+
+# Release 1
+
+meals = {
+  breakfast: "eggs",
+  lunch: "sandwich",
+  dinner: "pizza"
+  }
+
+desserts = ["ice cream", "candy", "cake"]
+
+p meals
+p desserts
+
+meals.each do |meal, food|
+  puts "#{meal.upcase} - #{food.upcase}"
+end
+
+desserts.each do |food|
+  puts food.upcase
+end
+
+p meals
+p desserts
+
+desserts.map! do |food|
+  food.upcase
+end
+
+p desserts
