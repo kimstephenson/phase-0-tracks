cfbf16ac7dc5082a44921c0fa1b39212bd365fd7
Kim Stephenson <kmstephe@usc.edu>
2016-07-19 14:05:45 -0700

    Add Fibonacci sequence adder

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -5,4 +5,18 @@ def search_array(arr, number)
 end
 
 p search_array(arr, 32)
-p search_array(ar, 1)
+p search_array(arr, 1)
+
+def fibonacci_adder(x)
+  array = [0,1,1,2,3,5]
+  if x >= 7
+    (x-6).times do
+      array << (array[-1] + array[-2])
+      p array
+    end
+  else
+    p array[0...x]
+  end
+end
+
+fibonacci_adder(100)
