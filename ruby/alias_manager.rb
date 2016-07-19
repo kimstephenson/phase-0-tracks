2f418ab7b71c6af47ffc4d6f03fd217974f895a3
Kim Stephenson <kmstephe@usc.edu>
2016-07-18 19:05:30 -0700

    Write method to advance vowels one letter

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -4,14 +4,27 @@ def name_swapper(name)
   name.split(" ").reverse.join(" ")
 end
 
-#Change all of the vowels to the next vowel in aeiou
+#Change all of the vowels to the next vowel in
+#"aeiou"
+def vowel_changer(name)
+  vowel_string = "aeiou"
+  name_array = name.downcase.chars
+  new_name = []
+  name_array.each do |character|
+    if character == "u"
+      character = "a"
+      new_name << character
+    elsif vowel_string.include? character
+      character = vowel_string[vowel_string.index(character) +1]
+      new_name << character
+    else
+      new_name << character
+    end
+  end
+  p new_name.join("").capitalize
+end
+vowel_changer("LUSITANIA")
 
 #Change all of the consonants to the next consonant
 
 #Create a method that takes a name and implements all of the changes
-
-def alias_maker(name)
-  puts name_swapper(name)
-end
-
-alias_maker("Kim Stephenson")
