002cb6ca5a6d904d1627b10b62e6ffea5646d4d7
Kim Stephenson <kmstephe@usc.edu>
2016-07-27 13:22:33 -0700

    Change module to a mixin module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,38 @@
+#module Shout
+#  def self.yell_angrily(words)
+#    words + "!!!" + ":("
+#  end
+
+#  def self.yell_happily(words)
+#    words + "!!!" + ":)"
+#  end
+#end
+
+#p Shout.yell_angrily("Hello")
+#p Shout.yell_happily("Hello")
+
 module Shout
-  def self.yell_angrily(words)
+  def yell_angrily(words)
     words + "!!!" + ":("
   end
 
-  def self.yell_happily(words)
+  def yell_happily(words)
     words + "!!!" + ":)"
   end
 end
 
-p Shout.yell_angrily("Hello")
-p Shout.yell_happily("Hello")
+class Boss
+  include Shout
+end
+
+class Cheerleader
+  include Shout
+end
+
+boss = Boss.new
+p boss.yell_angrily("You're fired")
+p boss.yell_happily("You're hired")
+
+cheerleader = Cheerleader.new
+p cheerleader.yell_happily("Go team")
+p cheerleader.yell_angrily("Booo")
