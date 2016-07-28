f95afe87d5b8ade5b31535437916ab3cd6314712
Kim Stephenson <kmstephe@usc.edu>
2016-07-28 01:40:20 -0700

    Modify class attributes

--- ruby/week_6_game/word_game.rb
+++ ruby/week_6_game/word_game.rb
@@ -5,8 +5,7 @@
 #  -A count of available guesses, which depends on the length of the word
 # -A boolean denoting whether the game is over (???)
 #The class will have the following instance methods:
-# -A method that splits the word into characters and calculates total available
-# guesses
+# -A method that splits the word into characters
 # -A method that checks if the guessed letter is in the word and keeps track
 # of the amount of guesses (only adding if the letter hasn’t been guessed already)
 # -A method that prints a string of blanks the length of the answer, and fills in
@@ -15,13 +14,16 @@
 # -Ask Player 1 for the word
 # -Print a statement that tells Player 2 how many guesses they have
 # -A loop that continually accepts guesses until the word is complete or they run
-# out of guesses. Also prints the updated feedback after each guess
+# out of guesses. Also prints the updated feedback after each guess, and how many guesses they've used
 # -A congratulatory message if they win, or a taunting message if they lose
 
-class WordGame(word)
-  def initialize
+class WordGame
+  attr_reader :available_guesses, :guesses_made
+
+  def initialize(word)
     @word = word
-    @available_guesses = word.length
+    @available_guesses = word.length * 2
     @guesses_made = 0
   end
 end
+
