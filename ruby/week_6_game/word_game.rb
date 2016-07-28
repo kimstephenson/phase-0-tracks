98c0b124f542a5c7fe53cff276e6282e8a68145b
Kim Stephenson <kmstephe@usc.edu>
2016-07-28 10:20:50 -0700

    Add and test method that keeps track of guesses made

--- ruby/week_6_game/word_game.rb
+++ ruby/week_6_game/word_game.rb
@@ -3,9 +3,9 @@
 # -A word, which is a writable attribute received in the form of user input
 # -A count of the number of guesses made
 #  -A count of available guesses, which depends on the length of the word
-# -A boolean denoting whether the game is over (???)
+# -An array of the letters in the word
+# -A boolean denoting whether the game is over
 #The class will have the following instance methods:
-# -A method that splits the word into characters
 # -A method that checks if the guessed letter is in the word and keeps track
 # of the amount of guesses (only adding if the letter hasn’t been guessed already)
 # -A method that prints a string of blanks the length of the answer, and fills in
@@ -18,12 +18,42 @@
 # -A congratulatory message if they win, or a taunting message if they lose
 
 class WordGame
-  attr_reader :available_guesses, :guesses_made
+  attr_reader :available_guesses, :guesses_made, :game_over, :letters_guessed
 
   def initialize(word)
     @word = word
+    @word_chars = word.split("")
     @available_guesses = word.length * 2
     @guesses_made = 0
+    @letters_guessed = []
+    @game_over = false
+  end
+
+  def guess_counter(letter)
+    unless @letters_guessed.include?(letter)
+      @letters_guessed << letter
+      @guesses_made += 1
+    else
+      @guesses_made
+    end
   end
 end
+
+game = WordGame.new("potato")
+p game.guess_counter("a")
+p game.letters_guessed
+p game.guess_counter("b")
+p game.letters_guessed
+p game.guess_counter("a")
+p game.letters_guessed
+p game.guess_counter("c")
+p game.letters_guessed
+
+
+
+
+
+
+
+
 
--- ruby/week_6_game/word_game_spec.rb
+++ ruby/week_6_game/word_game_spec.rb
@@ -3,4 +3,10 @@ require_relative "word_game"
 describe WordGame do
   let(:game) {WordGame.new("potato")}
 
+  it "keeps track of amount of guesses used" do
+    expect(game.guess_counter("a")).to eq(1)
+    expect(game.guess_counter("b")).to eq(2)
+    expect(game.guess_counter("a")).to eq(2)
+  end
+
 end
