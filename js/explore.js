1e198bda2d80a008582d17b09ae1532235bcb8e1
Kim Stephenson <kmstephe@usc.edu>
2016-08-02 12:04:33 -0700

    Add conditional that prints variable

--- js/explore.js
+++ js/explore.js
@@ -1,11 +1,14 @@
 //Take a string as an argument. Loop through it backwards, adding each letter to a new string. Print the new string.
 function reverse(string) {
-  revString = ""
+  var revString = ""
   for(var i = string.length; i > 0; i--){
     revString += string[i-1];
   }
   return revString;
 }
 
-newReverse = reverse("hello");
-console.log(newReverse)
+var newReverse = reverse("hello");
+
+if (1 == 1) {
+  console.log(newReverse);
+}
