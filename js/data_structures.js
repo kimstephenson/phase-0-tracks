0579b9b761b1cae1f49aff7ea92ddb75f2be8d89
Kim Stephenson <kmstephe@usc.edu>
2016-08-02 13:46:48 -0700

    Complete pairing exercise

--- js/data_structures.js
+++ js/data_structures.js
@@ -12,4 +12,28 @@ function createObject(arr1, arr2) {
   return namesColors;
 }
 
-createObject(colors, names);
+console.log(createObject(colors, names));
+
+function Car(make, model, has_sunroof){
+  console.log("Our new car: ");
+  this.make = make;
+  this.model = model;
+  this.has_sunroof = has_sunroof;
+
+  this.honk = function() {console.log("Beep!"); };
+  this.print = function() {
+    console.log("Make: " + this.make);
+    console.log("Model: " + this.model);
+    console.log("has_sunroof: " + this.has_sunroof);
+
+  };
+
+}
+
+var newCar = new Car("Infiniti", "FX35", true);
+newCar.honk();
+newCar.print();
+console.log("----------")
+var secondCar = new Car("Audi", "A4", false);
+secondCar.honk;
+secondCar.print();
