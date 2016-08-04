b17b06a64557678d97742acce0341abee3d4bc9c
Kim Stephenson <kmstephe@usc.edu>
2016-08-04 11:10:25 -0700

    Pseudocode the function

---
+++ js/algos.js
@@ -0,0 +1,8 @@
+//Take an array of words/phrases as an argument
+//Count the number of characters in each word/phrase, and return those lengths into a new array
+//Find the index of the largest number in the number array
+//Return the word that is at that same index in the word array
+
+function longestPhrase {
+
+}
