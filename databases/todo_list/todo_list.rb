16bd0d9e74b3de921be5ce9219624381e0006ed2
Kim Stephenson <kmstephe@usc.edu>
2016-08-12 15:23:40 -0700

    Create a menu that allows users to navigate the app

--- databases/todo_list/todo_list.rb
+++ databases/todo_list/todo_list.rb
@@ -10,6 +10,16 @@
 require "sqlite3"
 db = SQLite3::Database.new("todo_list.db")
 
+def print_menu
+  puts "Enter '1' to view your to-do list,"
+  puts "'2' to add an item,"
+  puts "'3' to update an item,"
+  puts "'4' to remove an item,"
+  puts "'5' to switch users,"
+  puts "'menu' to view this menu again,"
+  puts "or 'q' to quit the program."
+end
+
 puts "Welcome to the To-Do List Manager!"
 puts "Please enter your name:"
 user_name = gets.chomp
@@ -25,5 +35,17 @@ SQL
 
 db.execute(create_table)
 puts "Hello, #{user_name}."
+print_menu
+menu_input = ""
+until menu_input == "q"
+  menu_input = gets.chomp
+  if menu_input == "menu"
+    print_menu
+  elsif menu_input == "1"
+    puts "list will go here"
+  end
+end
+
+
 
 
