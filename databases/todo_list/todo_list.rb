5823f1f61f0615f1bc3fd65069d5a482e2c1478c
Kim Stephenson <kmstephe@usc.edu>
2016-08-12 13:53:50 -0700

    Pseudocode the to-do list app

---
+++ databases/todo_list/todo_list.rb
@@ -0,0 +1,8 @@
+#Allow users to:
+#     Add tasks to a to-do list
+#     Lists the time the task was added
+#     User can optionally add deadline (date & time)
+#     Can also optionally add a comment
+#     Remove tasks
+#     Print the list of tasks, in order of deadline. If no deadline added,
+#     those come last.
