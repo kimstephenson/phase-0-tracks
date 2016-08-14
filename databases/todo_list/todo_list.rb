c616bbdf1dee618b348efb1e577f0b9e6f38c04e
Kim Stephenson <kmstephe@usc.edu>
2016-08-14 13:28:38 -0700

    Add formatting to output

--- databases/todo_list/todo_list.rb
+++ databases/todo_list/todo_list.rb
@@ -22,40 +22,50 @@ end
 def view_list
   list_data = $Todo_Data.execute("SELECT * FROM #{$User_name} ORDER BY deadline ASC")
   puts "#{$User_name}'s To-Do List"
+  puts "---------------------"
   list_data.each do |task|
     puts "-#{task[1]}. Deadline: #{task[2]}. #{task[3]}"
   end
+  puts ""
   puts "Ready to perform another task."
+  puts ""
 end
 
 def add_item
-  puts "Enter the name of the task:"
+  puts "Enter the name of the item:"
   name = gets.chomp
-  puts "Enter the task deadline (YYYY-MM-DD HH:MM):"
+  puts "Enter the deadline (YYYY-MM-DD HH:MM):"
   deadline = gets.chomp
   puts "Enter a note or comment (optional):"
   comment = gets.chomp
-  $Todo_Data.execute("INSERT INTO #{$User_name} (task_name, deadline, opt_comment) VALUES (?, ?, ?)", [name, deadline, comment])
+  $Todo_Data.execute("INSERT INTO #{$User_name} (name, deadline, comment) VALUES (?, ?, ?)", [name, deadline, comment])
   puts "You added the item '#{name}' with deadline #{deadline}."
   puts "Ready to perform another task."
+  puts ""
 end
 
 def update_item
   puts "Your current list includes:"
-  puts $Todo_Data.execute("SELECT task_name FROM #{$User_name}")
+  puts $Todo_Data.execute("SELECT name FROM #{$User_name}")
   puts "Which item would you like to update?"
   item_to_update = gets.chomp
-  puts "Update item 'name', 'deadline' or 'comment'?"
+  puts "Update 'name', 'deadline' (YYYY-MM-DD HH:MM), or 'comment'?"
+  attr_to_update = gets.chomp.downcase
+  puts "Enter updated info:"
+  updated_info = gets.chomp
 end
 
 def remove_item
   puts "Your current list includes:"
-  puts $Todo_Data.execute("SELECT task_name FROM #{$User_name}")
+  puts $Todo_Data.execute("SELECT name FROM #{$User_name}")
+  puts ""
   puts "Which item would you like to remove?"
   item_to_remove = gets.chomp
-  $Todo_Data.execute("DELETE FROM #{$User_name} WHERE task_name LIKE '%#{item_to_remove}%'")
+  $Todo_Data.execute("DELETE FROM #{$User_name} WHERE name = '#{item_to_remove}'")
+  puts ""
   puts "Your updated list includes:"
-  puts $Todo_Data.execute("SELECT task_name FROM #{$User_name}")
+  puts $Todo_Data.execute("SELECT name FROM #{$User_name}")
+  puts ""
   puts "Ready to perform another task."
 end
 
@@ -66,18 +76,19 @@ $User_name = gets.chomp
 create_table = <<-SQL
   CREATE TABLE IF NOT EXISTS #{$User_name}(
     id INTEGER PRIMARY KEY,
-    task_name VARCHAR(255),
+    name VARCHAR(255),
     deadline DATETIME,
-    opt_comment VARCHAR(255)
+    comment VARCHAR(255)
     )
 SQL
 
 $Todo_Data.execute(create_table)
 puts "Hello, #{$User_name}."
+puts ""
 print_menu
 menu_input = ""
 until menu_input == "q"
-  menu_input = gets.chomp
+  menu_input = gets.chomp.downcase
   if menu_input == "menu"
     print_menu
   elsif menu_input == "1"
