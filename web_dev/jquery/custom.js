1b838952b40455fb7a5725e38f2b71c503b885c6
Kim Stephenson <kmstephe@usc.edu>
2016-08-16 16:15:45 -0700

    Add notes about research process

---
+++ web_dev/jquery/custom.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function() {
+   $("h1").click(function() {
+    alert("Dialog box created by jQuery");
+  });
+});
+
+$(document).ready(function(){
+  $("button").click(function() {
+    $("#myList").hide();
+  });
+});
